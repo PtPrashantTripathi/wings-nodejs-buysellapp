32e79eacc1600945bf8a1876bd5c8355d38719c2
ptprashanttripathi <ptprashanttripathi@outlook.com>
2022-07-08 20:26:15 +0530

    get method done

--- NodeJS/src/routers/sellAndBuy.js
+++ NodeJS/src/routers/sellAndBuy.js
@@ -7,6 +7,40 @@ const sellAndBuyRouter = new express.Router();
 
 // code goes here for routes
 
+sellAndBuyRouter.get("/sellProduct", (req, res) => {
+    let query = {};
+    let sortBy = null;
+    if (req.query.product) {
+        query.productName = req.query.product;
+    }
+    if (req.query.sortBy) {
+        switch (req.query.sortBy) {
+            case "lowerCostPrice":
+                sortBy = { costPrice: 1 }
+                break;
+            case "higherCostPrice":
+                sortBy = { costPrice: -1 }
+                break;
+            case "lowerSoldPrice":
+                sortBy = { soldPrice: 1 }
+                break;
+            case "higherSoldPrice":
+                sortBy = { soldPrice: -1 }
+                break;
+            default:
+                null;
+        }
+    }
+
+    SellBuy.find(query).sort(sortBy)
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            res.status(400).json({ message: err.message });
+        });
+});
+
 // exporting the router
 
 module.exports = sellAndBuyRouter;
